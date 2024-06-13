acf152a1b9cc04c7d69cd1033f7e0f4deaf20c11
Elena Papanikolas <epapanikolas@hotmail.com>
2024-06-13 13:14:46 -0600

    Added validation to questions

--- index.js
+++ index.js
@@ -9,63 +9,125 @@ const questions = [
     {
         type: "input",
         name: "name",
-        message: "Welcome to the README generator! Please enter your full name:"
+        message: "Welcome to the README generator! Please enter your full name:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter your full name"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "github",
         message: "Enter your GitHub username:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter a valid GitHub username"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "email",
         message: "Enter your email:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter a valid email"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "title",
         message: "Enter the title of your project:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter the title of your project"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "description",
         message: "Enter a short description of your project:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter a description of your project"
+            }
+            return true;
+        }
     },
     {
         type: "list",
         name: "license",
         message: "Select a license for your project:",
-        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        validate: function(input) {
+            if (!input) {
+                return "Please select a valid license option"
+            }
+            return true;
+        }
+        
     },
     {
         type: "input",
-        name: "dependencies",
+        name: "installation",
         message: "Enter instructions to install dependencies:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter instructions to install or write N/A"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "usage",
-        message: "Enter instructions for usage:",
+        message: "Enter what the user needs to know about using this project:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter usage information about your project"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "tests",
         message: "Enter instructions needed to run tests:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter the instructions to run tests or write N/A"
+            }
+            return true;
+        }
     },
     {
         type: "input",
-        name: "know how",
-        message: "Enter anything the user needs to know about using the repo:",
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "Enter anything the user needs to know about contributing to the repo:",
+        name: "contributions",
+        message: "Enter anything the user needs to know about contributing to the project;",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter information about contributing to your project or write N/A"
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "credits",
         message: "Enter any credits and acknowledgements for your project here:",
+        validate: function(input) {
+            if (!input) {
+                return "Please enter any credits/acknowledgements or write N/A"
+            }
+            return true;
+        }
     }
 ];
 
@@ -73,7 +135,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
